4138bd79756ec8fecb49a4184c9d14b2176cc07c
Ghazi Triki <ghazi.triki@riadvice.com>
2026-09-06 13:38:05 +0100

    Add tests for link and image tracking in the message parser

--- spec/lib/postal/message_parser_spec.rb
+++ spec/lib/postal/message_parser_spec.rb
@@ -23,6 +23,46 @@ describe Postal::MessageParser do
     expect(parser.tracked_links).to eq 1
   end
 
+  it "should rewrite links and insert a tracking image in HTML messages" do
+    domain = create(:domain, owner: server)
+    message = MessageFactory.outgoing(server, domain: domain) do |_msg, mail|
+      mail.html_part = Mail::Part.new do
+        content_type "text/html; charset=UTF-8"
+        body %(<html><body><p><a href="https://github.com/atech/postal">Postal</a></p></body></html>)
+      end
+    end
+    create(:track_domain, server: server, domain: domain)
+    parser = Postal::MessageParser.new(message)
+    expect(parser.actioned?).to be true
+    expect(parser.tracked_links).to eq 1
+    expect(parser.tracked_images).to eq 1
+    expect(parser.new_body).to include("href='https://click.#{domain.name}/")
+    expect(parser.new_body).to include("class='ampimg'")
+  end
+
+  it "should not rewrite links for excluded click domains" do
+    message = create_plain_text_message(server, "Hello world! http://github.com/atech/postal", "test@example.com")
+    create(:track_domain, server: server, domain: message.domain, excluded_click_domains: "github.com")
+    parser = Postal::MessageParser.new(message)
+    expect(parser.tracked_links).to eq 0
+    expect(parser.new_body).to include("http://github.com/atech/postal")
+  end
+
+  it "should not rewrite links or insert images when tracking is disabled on the track domain" do
+    domain = create(:domain, owner: server)
+    message = MessageFactory.outgoing(server, domain: domain) do |_msg, mail|
+      mail.html_part = Mail::Part.new do
+        content_type "text/html; charset=UTF-8"
+        body %(<html><body><a href="https://github.com/atech/postal">Postal</a></body></html>)
+      end
+    end
+    create(:track_domain, server: server, domain: domain, track_clicks: false, track_loads: false)
+    parser = Postal::MessageParser.new(message)
+    expect(parser.actioned?).to be false
+    expect(parser.tracked_links).to eq 0
+    expect(parser.tracked_images).to eq 0
+  end
+
   it "should strip the +notrack marker even when the track domain's DNS isn't OK" do
     message = create_plain_text_message(server, "Hello world! http+notrack://github.com/atech/postal", "test@example.com")
     create(:track_domain, server: server, domain: message.domain, dns_status: "Missing")
